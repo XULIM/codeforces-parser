783d1b3d5b70cd46968b84162c090fca513a82eb
bobillibo <not3lbobillibo@gmail.com>
2024-02-22 00:56:09 -0500

    added method to parse a single problem

--- parse1.py
+++ parse1.py
@@ -15,7 +15,7 @@ class class_parameter(Enum):
     NAME = "problemsetName"
 
 
-class parser():
+class parser:
     def __init__(self):
         self.API = "https://codeforces.com/api/"
 
@@ -26,14 +26,17 @@ class parser():
         if res.status_code == 200:
             res_js = res.json()
         elif res.status_code == 400:
-            comment = res.json()['comment']
+            comment = res.json()["comment"]
             raise InvalidArgumentException(f"Invalid Argument: {comment}")
         else:
             raise InvalidURLException(url, res.status_code)
-        return res_js['result']
+        return res_js["result"]
 
     def parse_with_parameter(self, method, params: dict):
         param_str = ""
         for key, value in params.items():
             param_str += "{0}={1}".format(key, (str(i) + "&" for i in value))
-        return self.parse_method(method+params)
+        return self.parse_method(method + params)
+
+    def get_problem(self, contest_id: int, problem_index: str):
+        pass
