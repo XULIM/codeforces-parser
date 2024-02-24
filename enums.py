394ec63f2477d491f87e105ac4e5126acaf1866f
bobillibo <not3lbobillibo@gmail.com>
2024-02-23 21:09:14 -0500

    added documentation

--- enums.py
+++ enums.py
@@ -8,6 +8,10 @@ class ExtendedEnum(Enum):
 
 
 class cf_problem_field(ExtendedEnum):
+    """
+    Represents the the fields of a problem object from CodeForces.
+    """
+
     CONTEST_ID = "contestId"
     PROBLEMSET_NAME = "problemsetName"
     ID = "index"
@@ -19,14 +23,27 @@ class cf_problem_field(ExtendedEnum):
 
 
 class tables(ExtendedEnum):
+    """
+    Represents the tables created by the database.
+    """
+
     PROBLEMS = "problems"
+    CONTEST = "contest"
 
 
 class methods(ExtendedEnum):
+    """
+    Represents the available URL methods when parsing the CodeForces API.
+    """
+
     PROBLEM_SET = "problemset.problems?"
     PROBLEM_SET_STATUS = "problemset.recentStatus?"
 
 
 class class_parameter(ExtendedEnum):
+    """
+    Represents the available URL parameters when parsing the CodeForces API.
+    """
+
     TAGS = "tags"
     NAME = "problemsetName"
