bd6313cd5248b58f4be97d092a4bc8dea5b0db1c
bobillibo <not3lbobillibo@gmail.com>
2024-03-11 16:39:32 -0400

    modified parser class for simplicity

--- parse1.py
+++ parse1.py
@@ -1,44 +1,41 @@
 from bs4 import BeautifulSoup as bs
 import requests
 from exceptions import InvalidURLException, InvalidArgumentException
-from database import tables
+from enums import methods, class_parameter
 
 
 class parser:
     def __init__(self):
         self.API = "https://codeforces.com/api/"
 
-    def parse_method(self, suffix: str):
+    def parse_method(self, method: methods, params: dict[str, list[str]] | None = None):
         """
         Parses the CodeForces API with the according method string.
 
-        raises exceptions.InvalidArgumentException when given an invalid argument (suffix string).
-        raises exceptions.InvalidURLException when given an invalid URL (caused by suffix string).
+        raises exceptions.InvalidArgumentException when given an invalid argument (method or params).
+        raises exceptions.InvalidURLException when given an invalid URL (caused by method or params).
         """
         res_js = None
-        url = self.API + suffix
+        url: str = self.API
+        query: str = str(method.value)
+        if params:
+            for item in params.items():
+                query += item[0] + "&".join(item[1])
+        url += query
+        print("Querying URL: ", url)
         res = requests.get(url)
         if res.status_code == 200:
+            print("Accessing data...")
             res_js = res.json()
         elif res.status_code == 400:
+            print("Something went wrong.")
             comment = res.json()["comment"]
             raise InvalidArgumentException(f"Invalid Argument: {comment}")
         else:
             raise InvalidURLException(url, res.status_code)
+        print("Successfully parsed data.")
         return res_js["result"]
 
-    def parse_with_parameter(self, method: str, params: dict):
-        """
-        Parses the CodeForces API with the according method and parameters.
-
-        raises exceptions.InvalidArgumentException when given an invalid argument (methods, params).
-        raises exceptions.InvalidURLException when given an invalid URL (caused by methods, params).
-        """
-
-        param_str = ""
-        for key, value in params.items():
-            param_str += "{0}={1}".format(key, (str(i) + "&" for i in value))
-        return self.parse_method(method + param_str)
-
+    # TODO: select from database then use bs4 to parse content from website
     def get_problem(self, contest_id: int, problem_index: str):
         pass
