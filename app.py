d2e03b0ee3e05d9c79c1b8cc108fd01efac932ef
bobillibo <not3lbobillibo@gmail.com>
2024-03-11 16:38:50 -0400

    added files app.py, misc_utils.py

---
+++ app.py
@@ -0,0 +1,8 @@
+from enums import tables, class_parameter, methods
+from parse1 import parser
+from database import parser_database
+
+
+class app:
+    def __init__(self) -> None:
+        pass
---
+++ misc_utils.py
@@ -0,0 +1,6 @@
+import colorama
+
+
+class spinner:
+    def __init__(self) -> None:
+        self.sp = "/-\\|"
