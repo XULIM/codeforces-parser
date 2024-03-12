6a8b9bcd2814e0998503dccd75ba0231e5498581
bobillibo <not3lbobillibo@gmail.com>
2024-03-12 14:51:28 -0400

    finished spinner method in loading helper class

--- misc_utils.py
+++ misc_utils.py
@@ -1,6 +1,13 @@
 import colorama
+import time
 
 
-class spinner:
-    def __init__(self) -> None:
-        self.sp = "/-\\|"
+class loading:
+    @staticmethod
+    def spinner(flag) -> None:
+        sp = "-\\|/"
+        i: int = 0
+        while flag:
+            i = (i + 1) % 4
+            print(sp[i], end="\r")
+            time.sleep(0.5)
