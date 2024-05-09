2e685399a238543dcf0b2c8c8b2ada6c1e6ec71a
bobillibo <not3lbobillibo@gmail.com>
2024-05-09 16:20:36 -0400

    minor update

--- main.py
+++ main.py
@@ -4,10 +4,9 @@ from database import parser_database
 from enums import methods, class_parameter
 import atexit
 from colorama import Fore, Back, Style
-import pprint
 import asyncio
-import aiohttp
 import os
+from pprint import pprint
 
 
 @atexit.register
@@ -21,25 +20,14 @@ def err(msg: str):
     print(Fore.RED + msg)
 
 
-def set_env() -> None:
-    print(os.environ)
-
-
 async def main():
     try:
         # db = parser_database()
         ps = parser()
         params = {str(class_parameter.TAGS.value): ["implementation", "math"]}
-        val = await ps.parse_async(method=methods.PROBLEM_SET, params=params)
-        for entry in val["result"]["problemStatistics"]:
-            print(entry)
-        # async with aiohttp.ClientSession() as session:
-        #     async with session.get(
-        #         f"https://codeforces.com/api/{str(methods.PROBLEM_SET)}"
-        #     ) as res:
-        #         print(res.status)
-        #         res_js = await res.json()
-        #         pprint.pprint(res_js["result"])
+        val = await ps.parse(method=methods.PROBLEM_SET, params=params)
+        for entry in val["result"]["problems"]:
+            pprint(entry)
 
     except InvalidURLException:
         err("Invalid URL: check if it is a valid format.")
