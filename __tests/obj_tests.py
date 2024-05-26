19227b2e45ffb59578068d8ae93f51c9c36f9cac
XULIM <not3lbobillibo@gmail.com>
2024-05-26 19:32:43 -0400

    add entry object initialization test

--- __tests/obj_tests.py
+++ __tests/obj_tests.py
@@ -1,24 +1,22 @@
 from objects import entry
 
+
 def entry_initializable():
     fake_problem = {
         "contestId": 1,
-        "index": 'A',
+        "index": "A",
         "name": "hello world",
         "type": "PROGRAMMING",
         "rating": 800,
-        "tags": ['implementation', 'math']
+        "tags": ["implementation", "math"],
     }
-    fake_stats = {
-        "contestId": 1,
-        "index": 'A',
-        "solvedCount": 1500
-    }
+    fake_stats = {"contestId": 1, "index": "A", "solvedCount": 1500}
     en = entry(fake_problem, fake_stats)
-    assert(en.contest_id==1 and \
-        en.index=='A' and \
-        en.name=="hello world" and\
-        en.rating==800 and\
-        en.tags==['implementation', 'math'] and\
-        en.solved_count==1500
+    assert (
+        en.contest_id == 1
+        and en.index == "A"
+        and en.name == "hello world"
+        and en.rating == 800
+        and en.tags == ["implementation", "math"]
+        and en.solved_count == 1500
     )
