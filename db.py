6d07496c78e8a6f5883e73025705a750654f79a9
XULIM <not3lbobillibo@gmail.com>
2024-06-03 12:05:26 -0400

    update new database class with insert operations

--- db.py
+++ db.py
@@ -5,31 +5,84 @@ from objects import entry, entries
 class db:
     def __create_table(self):
         self.cur.execute(
-                "CREATE TABLE IF NOT EXISTS problems ("
-                    "contestId INTEGER NOT NULL,"
-                    "problemIndex VARCHAR NOT NULL,"
-                    "rating INT,"
-                    "tags TEXT,"
-                    "solvedCount INT,"
-                    "PRIMARY KEY (contestId, problemIndex)"
-                ");"
-            )
+            "CREATE TABLE IF NOT EXISTS problems ("
+            "contestId INTEGER NOT NULL,"
+            "problemIndex VARCHAR NOT NULL,"
+            "rating INT,"
+            "tags TEXT,"
+            "solvedCount INT,"
+            "PRIMARY KEY (contestId, problemIndex)"
+            ");"
+        )
 
-    def __init__(self, file="results.db"):
+    def drop_table(self, table="problems"):
+        self.cur.execute("DROP TABLE IF EXISTS ?;", table)
+
+    def get_tables(self):
+        try:
+            if not self.con:
+                self.con = sql.connect(self.file)
+                self.cur = self.con.cursor()
+            self.cur.execute("SELECT name FROM sqlite_master;")
+        except sql.OperationalError as e:
+            print("Error selecting tables: " + str(e))
+        except sql.Error as e:
+            print("Error selecting tables: " + str(e))
+        finally:
+            res = self.cur.fetchone()
+            if self.con:
+                self.con.close()
+            return res
+
+    def __init__(self, file="results.db") -> None:
         self.file = file
         self.con = sql.connect(file)
         self.cur = self.con.cursor()
         try:
             self.__create_table()
-        except sql.OperationalError as e:
-            print("Erorr creating problems table.")
+        except sql.OperationalError:
+            print("Error creating problems table.")
         except sql.Error as e:
             print("Error: " + str(e))
         finally:
+            print(self.con)
             if self.con:
+                self.cur.close()
                 self.con.close()
+                self.con = None
 
-    def insert(self, ens: entries):
-        if not self.con:
-            self.con = sql.connect(self.file)
+    def insert(self, en: entry) -> None:
+        try:
+            if not self.con:
+                self.con = sql.connect(self.file)
+                self.cur = self.con.cursor()
+            self.cur.execute("INSERT INTO problems VALUES(?, ?, ?, ?, ?);", en.conform())
+            self.con.commit()
+        except sql.OperationalError:
+            print("Error inserting into table: " + str(en))
+        except sql.Error as e:
+            print("Error inserting into table: " + str(e))
+        finally:
+            if self.con:
+                self.cur.close()
+                self.con.close()
+                self.con = None
 
+    def insert_many(self, ens: entries) -> None:
+        try:
+            if not self.con:
+                self.con = sql.connect(self.file)
+                self.cur = self.con.cursor()
+            self.cur.executemany(
+                "INSERT INTO problems VALUES(?,?,?,?,?)", ens.conform()
+            )
+            self.con.commit()
+        except sql.OperationalError:
+            print("Error inserting into table: " + str(ens))
+        except sql.Error as e:
+            print("Error inserting into table: " + str(e))
+        finally:
+            if self.con:
+                self.cur.close()
+                self.con.close()
+                self.con = None
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import atexit
 import asyncio
 import sqlite3
+from aiohttp import ClientSession
+import json
 from parser import parser
+from db import db
 from database import parser_database
 from objects import entry, entries
 from enums import methods, class_parameter
@@ -15,25 +18,25 @@ def exit_handler() -> None:
 
 async def main():
     try:
-        con = sqlite3.connect("results.db")
-        cur = con.cursor()
-        print("Creating: problems table")
-        cur.execute("""
-        DROP TABLE IF EXISTS problems;
-        """)
-        cur.execute(
-            """
-                CREATE TABLE IF NOT EXISTS problems (
-                    contestId INTEGER NOT NULL,
-                    problemIndex VARCHAR NOT NULL,
-                    rating INT,
-                    tags TEXT,
-                    solvedCount INT,
-                    PRIMARY KEY (contestId, problemIndex)
-                );
-            """
-        )
-        print("Created table: problems")
+        # con = sqlite3.connect("results.db")
+        # cur = con.cursor()
+        # print("Creating: problems table")
+        # cur.execute("""
+        # DROP TABLE IF EXISTS problems;
+        # """)
+        # cur.execute(
+        #     """
+        #         CREATE TABLE IF NOT EXISTS problems (
+        #             contestId INTEGER NOT NULL,
+        #             problemIndex VARCHAR NOT NULL,
+        #             rating INT,
+        #             tags TEXT,
+        #             solvedCount INT,
+        #             PRIMARY KEY (contestId, problemIndex)
+        #         );
+        #     """
+        # )
+        # print("Created table: problems")
 
         # print("Getting tables")
         # cur.execute("SELECT name FROM sqlite_master;")
@@ -43,15 +46,17 @@ async def main():
         # cur.execute("PRAGMA table_info(problems)")
         # print(f"Table info: {cur.fetchall()}")
 
+        d = db()
         ps = parser()
         params = {str(class_parameter.TAGS.value): ["implementation", "math"]}
         val = await ps.parse(method=methods.PROBLEM_SET)
-        cur.executemany("INSERT INTO problems VALUES (?, ?, ?, ?, ?)", val.conform())
-        con.commit()
+        # cur.executemany("INSERT INTO problems VALUES (?, ?, ?, ?, ?)", val.conform())
+        # con.commit()
+        # print("done")
 
-        cur.execute("SELECT * FROM problems;")
-        print(cur.fetchall())
-        con.close()
+        # cur.execute("SELECT * FROM problems;")
+        # print(cur.fetchall())
+        # con.close()
     except InvalidURLException:
         print("Invalid URL: check if it is a valid format.")
     except InvalidArgumentException:
