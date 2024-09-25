e9bde1d2fc9a4449eaa5d6659a6f6913d0f945bc
XULIM <not3lbobillibo@gmail.com>
2024-09-24 22:42:07 -0400

    Finalising p1

--- main.py
+++ main.py
@@ -1,49 +1,37 @@
 import atexit
 import asyncio
-import os
-from time import asctime, localtime
+from time import asctime, localtime, strftime, sleep
 from typing import NoReturn
 
 from aiohttp import ClientSession
 from bs4 import BeautifulSoup
 from enums import methods
-from parser import CFParser
 from db import db
 from ua_parser import user_agent_parser as uap
 from ua import Rotator
-import p1
+from p1 import *
+
 
 @atexit.register
 def exit_handler() -> None:
     print("Exiting Programme.")
 
-def get_last_modified_time(file: str):
-    return localtime(os.path.getmtime(file))
-
-async def get_user_agents():
-    url = "https://www.useragentlist.net/"
-    user_agents = []
-    async with ClientSession() as sesh:
-        async with sesh.get(url) as res:
-            if res.status == 200:
-                soup = BeautifulSoup(await res.text(), "html.parser")
-                for user_agent in soup.select("pre.wp-block-code"):
-                    user_agents.append(user_agent.text)
-            else:
-                print("Error: ", res.status)
-
-    with open("./ua_list", "w") as f:
-        for ua in user_agents:
-            f.write(ua + "\n")
 
 async def main():
     try: 
-        print(asctime(get_last_modified_time("ua_list")))
-        input, output = await p1.get_tests(1998, "C")
-        print("input: ", input)
-        print("output: ", output)
+        prob1 = {
+            "contestId": 123,
+            "index": "C",
+            "name": "k.makise",
+            "points": 1000,
+            "rating": 1500,
+            "tags": ["implementation", "math"]
+        }
+        test_entry = Entry(prob1)
+        print(test_entry)
+        print(file_refresh(UA_PATH))
     except Exception as e:
-        print("Error: ", str(e))
+        log(Status.ERR, str(e))
 
 
 asyncio.run(main())
--- p1.py
+++ p1.py
@@ -1,8 +1,19 @@
+import os
+
 from typing import Any
 from aiohttp import ClientSession
 from bs4 import BeautifulSoup, Tag
 from ua import Rotator
 from enum import Enum
+from re import escape
+from time import time, sleep
+from datetime import timedelta
+
+UA_PATH = "ua_list"
+with open(UA_PATH, "r") as f:
+    USER_AGENTS = f.read().splitlines()
+ROTATOR = Rotator(USER_AGENTS)
+HTML_PATH = "index.html"
 
 class Status(Enum):
     OK = (0, "[OK]")
@@ -14,18 +25,93 @@ class Status(Enum):
     def __str__(self):
         return self.string
 
-with open("./ua_list", "r") as f:
-    USER_AGENTS = f.read().splitlines()
-ROTATOR = Rotator(USER_AGENTS)
-HTML_PATH = "index.html"
+class Endpoint(Enum):
+    Problems = "problemset.problems?"
+    Status = "problemset.recentStatus?"
+
+class Entry():
+    def __init__(self, problems: dict):
+        try:
+            self.contest_id = problems["contestId"]
+            self.index = problems["index"]
+            self.name = escape(problems.get("name", ""))
+            self.points = problems.get("points", 0)
+            self.rating = problems.get("rating", 0)
+            self.tags = problems.get("tags", [])
+            self.is_solved = False
+        except Exception as e:
+            log(Status.ERR, "from Entry.__init__: cannot create entry object.", 
+                "Details:", e)
+
+    def __str__(self):
+        return (f"{self.contest_id},"
+            f"{self.index}," +
+            f"{self.name}," +
+            f"{self.points}," +
+            f"{self.rating}," +
+            f"{self.tags}," +
+            f"{(1 if self.is_solved else 0)}"
+        )
+
+    def conform(self):
+        return self.__str__()
+
+def file_refresh(file: str, cd_days = 21, cd_hours = 0, cd_minutes = 0, cd_seconds = 0) -> bool:
+    cd = timedelta(days=cd_days, hours=cd_hours, minutes=cd_minutes, seconds=cd_seconds)
+    delta_t = timedelta(seconds=(time() - os.path.getmtime(file)))
+    diff = delta_t - cd
+    if diff.total_seconds():
+        return True
+    return False
 
 def log(stat: Status, *args):
     print(f"{str(stat)}:", "".join(args))
+
+async def get_user_agents():
+    url = "https://www.useragentlist.net/"
+    user_agents = []
+    sleep(1) # To avoid timeout due to spamming
+    try:
+        async with ClientSession() as sesh:
+            async with sesh.get(url) as res:
+                if res.status == 200:
+                    soup = BeautifulSoup(await res.text(), "html.parser")
+                    for user_agent in soup.select("pre.wp-block-code"):
+                        user_agents.append(user_agent.text)
+                else:
+                    print("Error: ", res.status)
+    except Exception as e:
+        return (Status.ERR, str(e))
+
+    try:
+        with open(UA_PATH, "w") as f:
+            for ua in user_agents:
+                f.write(ua + "\n")
+        return (Status.OK, user_agents)
+    except Exception as e:
+        return (Status.ERR, str(e))
+
+async def get_problems() -> tuple[Status, dict[str, Any]]:
+    api_url = f"https://codeforces.com/api/{Endpoint.Problems.value}"
+    header = {"User-Agent": str(ROTATOR.get())}
+    print(f"Parsing CodeForces API: {api_url}.")
+    sleep(1) # To avoid timeout due to spamming
+    async with ClientSession(headers=header) as session:
+        async with session.get(api_url) as res:
+            if res.status == 200:
+                log(Status.OK, f"from get_problems: able to access CodeForces API.")
+                results = await res.json()
+            else:
+                log(Status.ERR, f"from get_problems: unable to access CodeForces API", 
+                    f"({res.status}).")
+                return (Status.ERR, {})
+    return (Status.OK, results["result"]["problems"])
 
 async def get_page(contest_id: int, index: str) -> None:
     parse_url = f"https://codeforces.com/contest/{contest_id}/problem/{index}"
     print("Starting parsing on URL: ", parse_url)
     header = {"User-Agent": str(ROTATOR.get())}
+    sleep(1) # To avoid timeout due to spamming
     async with ClientSession(headers=header) as session:
         async with session.get(parse_url) as res:
             if res.status == 200:
