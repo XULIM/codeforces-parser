85cd472979f6bf351a02c8a06efb2cfd31bd8542
XULIM <not3lbobillibo@gmail.com>
2024-10-08 20:37:37 -0400

    Add file template for cpp

---
+++ templates/template.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+void solve()
+{
+
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int tt;
+    cin >> tt;
+    while (tt--)
+    {
+
+    }
+    return 0;
+}
