5d30f4456e60176c8f76f4c4b4d5622e9018629b
XULIM <not3lbobillibo@gmail.com>
2024-10-09 19:07:03 -0400

    Add create_tables and simple connection handler

--- d1.py
+++ d1.py
@@ -1,20 +1,90 @@
 import p1
 import sqlite3
+from sqlite3 import Connection, Cursor
 from enum import Enum
+from p1 import log, Status
 
 type void = None
 
-class Attributes(Enum):
-    ID = "contest_id",
-    INDEX = "index",
-    NAME = "name",
-    POINTS = "points",
-    RATING = "rating",
-    TAGS = "tags",
-    SOLVED = "is_solved"
+class Attributes(tuple, Enum):
+    def __new__(cls, values):
+        obj = tuple.__new__(cls, values[0])
+        obj._value_ = values[0]
+        return obj
+    ID = ("contest_id", "INTEGER NOT NULL"),
+    INDEX = ("pindex", "VARCHAR NOT NULL"),
+    NAME = ("name", "TEXT"),
+    POINTS = ("points", "INT"),
+    RATING = ("rating", "INT"),
+    TAGS = ("tags", "TEXT"),
+    SOLVED = ("is_solved", "INT"),
 
-con = sqlite3.connect("results.db")
-cur = con.cursor()
+DB_NAME = "results.db"
+TABLE_NAME = "problems"
 
-def create_table():
-    pass
+def doc_param(*sub):
+    """
+    Decorator that allows doc strings to accept parameters.
+    """
+    def dec(obj):
+        obj.__doc__ = obj.__doc__.format(*sub)
+        return obj
+    return dec
+
+@doc_param(DB_NAME)
+def establish() -> tuple[Connection, Cursor]:
+    """
+    Establishes sqlite3 connection to DB_NAME({0}).
+    Raises sqlite3.DatabaseError if fails.
+    Returns a tuple in the form of (sqlite3.Connection, sqlite3.Cursor).
+    """
+    try:
+        con = sqlite3.connect(DB_NAME)
+        cur = con.cursor()
+    except sqlite3.Error as e:
+        log(Status.ERR, f"could not establish sqlite3 connection on {DB_NAME},", str(e))
+        raise sqlite3.DatabaseError("Could not establish sqlite3 connection")
+    return (con, cur)
+
+def cc_close(con: Connection | None, cur: Cursor | None = None):
+    """
+    Closes sqlite3.Connection and sqlite3.Cursor if they are open.
+    """
+    if (cur):
+        cur.close()
+    if (con):
+        con.commit()
+        con.close()
+
+def drop_table() -> void:
+    con, cur = establish()
+    try:
+        cur.execute(f"DROP TABLE IF EXISTS ?;", TABLE_NAME)
+    except sqlite3.Error as e:
+        raise sqlite3.DatabaseError(f"Could not drop table {TABLE_NAME}.", str(e))
+    finally:
+        cc_close(con,cur)
+
+def create_table() -> void:
+    """
+    Creates the problems table with the attributes in Attributes enum.
+    Raises sqlite3.DatabaseError if fails.
+    """
+    # ---
+    cmd = "CREATE TABLE IF NOT EXISTS problems(\n"
+    for _, mem in Attributes.__members__.items():
+        attr, dtype = mem.value
+        cmd += f"\t{attr} {dtype},\n"
+    cmd += f"\tPRIMARY KEY ({Attributes.ID.value[0]}, {Attributes.INDEX.value[0]})\n"
+    cmd += ");"
+    # ---
+
+    con, cur = establish()
+    try:
+        cur.execute(cmd)
+    except sqlite3.DatabaseError as e:
+        log(Status.ERR, "from create_table: could not create table.", str(e))
+        raise sqlite3.DatabaseError("Could not create table", str(e))
+    finally:
+        cc_close(con,cur)
+    log(Status.OK, "from create_table: successfully created table [problems].")
