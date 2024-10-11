715c15628a73ba75b0fd596272e2a53413e008a5
XULIM <not3lbobillibo@gmail.com>
2024-10-11 03:38:40 -0400

    Add populate_table function to auto populate and update the table

--- main.py
+++ main.py
@@ -1,15 +1,40 @@
+import os
 import atexit
 import asyncio
-from lib import gen, db, ps
-from lib.ps import Status, log
+from lib import db, ps
+from lib.ps import Status, log, file_refresh
 
+type void = None
 
 @atexit.register
 def exit_handler() -> None:
     print("Exiting Programme.")
 
+# TODO: this function does not cover all edge cases.
+# The problems table could be created but without problems.
+async def populate_table(forced: bool = False) -> void:
+    # ---
+    async def make():
+        db.create_table()
+        status, problems = await ps.get_problems()
+        if (status is Status.OK):
+            db.insert(problems)
+        else:
+            log(Status.ERR, "from populate_table: ", "could not populate table - problems.")
+            return
+        log(Status.OK, "from populate_table: ", "table population successful.")
+    # ---
+    if (not forced or not file_refresh(db.DB_NAME, cd_days=3)):
+        if (not db.table_exists()):
+            await make()
+        else:
+            log(Status.OK, "from populate_table: ", "table exists and populated.")
+        return
+    os.remove(db.DB_NAME)
+    db.drop_table()
+    await make()
 
-async def main():
+async def main() -> void:
     con,cur = db.establish()
     try: 
         prob1 = {
@@ -21,14 +46,13 @@ async def main():
             "tags": ["implementation", "dp", "segment tree"]
         }
         entry = ps.dtot(prob1)
-        # db.drop_table()
-        # db.create_table()
-        # status, problems = await ps.get_problems()
-        # if (status == Status.OK):
-        #     db.insert(problems)
-        # for line in cur.execute("SELECT cid, pindex, name, rating, tags FROM problems;"):
-        #     print(line)
-        log(Status.WARN, "this is a warning message.", prob1, entry)
+        await populate_table(forced=True)
+        cid = 1420
+        found, li = db.select_contest(cid)
+        if (not found):
+            log(Status.WARN, f"could not find the contest {cid}.")
+        else:
+            print(li)
     except Exception as e:
         log(Status.ERR, str(e))
     finally:
