e90da5974752ad4c6192d525376e057b69d62b5b
XULIM <not3lbobillibo@gmail.com>
2024-10-11 03:37:50 -0400

    Update Attributes enum to use __init__ instead of __new__ and add table_exists function

--- lib/db.py
+++ lib/db.py
@@ -1,22 +1,36 @@
 import sqlite3
 from sqlite3 import Connection, Cursor
 from enum import Enum
-from ps import log, Status
+from lib.ps import log, Status
 
 type void = None
 
 class Attributes(tuple, Enum):
-    def __new__(cls, values):
-        obj = tuple.__new__(cls, values[0])
-        obj._value_ = values[0]
-        return obj
+    """
+    For each name there is a tuple value in the form of
+        (col_name, dtype)
+        where col_name is the column name in database,
+        and dtype is the datatype used upon table creation.
+
+    Members:
+        first (str): returns col_name.
+        second (str): returns dtype.
+    """
+    # def __new__(cls, values):
+    #     obj = tuple.__new__(cls, values[0])
+    #     obj._value_ = values[0]
+    #     return obj
+    def __init__(self, tup_val):
+        self._value_ = tup_val[0]
+        self.first = self._value_[0]
+        self.second = self._value_[1]
     ID = ("cid", "INTEGER NOT NULL"),
     INDEX = ("pindex", "VARCHAR NOT NULL"),
     NAME = ("name", "TEXT"),
     POINTS = ("points", "INT"),
     RATING = ("rating", "INT"),
     TAGS = ("tags", "TEXT"),
-    SOLVED = ("is_solved", "INT"),
+    SOLVED = ("solved", "INT"),
 
 DB_NAME = "results.db"
 TABLE_NAME = "problems"
@@ -56,6 +70,9 @@ def ccfree(con: Connection | None, cur: Cursor | None = None):
         con.close()
 
 def drop_table() -> void:
+    """
+    Drops the problems table.
+    """
     con, cur = establish()
     try:
         cur.execute(f"DROP TABLE IF EXISTS {TABLE_NAME};")
@@ -70,11 +87,11 @@ def create_table() -> void:
     Raises sqlite3.DatabaseError if fails.
     """
     # ---
-    cmd = "CREATE TABLE IF NOT EXISTS problems(\n"
+    cmd = f"CREATE TABLE IF NOT EXISTS {TABLE_NAME}(\n"
     for _, mem in Attributes.__members__.items():
         attr, dtype = mem.value
         cmd += f"\t{attr} {dtype},\n"
-    cmd += f"\tPRIMARY KEY ({Attributes.ID.value[0]}, {Attributes.INDEX.value[0]})\n"
+    cmd += f"\tPRIMARY KEY ({Attributes.ID.first}, {Attributes.INDEX.first})\n"
     cmd += ");"
     # ---
 
@@ -86,7 +103,26 @@ def create_table() -> void:
         raise sqlite3.DatabaseError("Could not create table", str(e))
     finally:
         ccfree(con,cur)
-    log(Status.OK, "from create_table: successfully created table [problems].")
+    log(Status.OK, f"from create_table: successfully created table [{TABLE_NAME}].")
+
+def table_exists() -> bool:
+    con, cur = establish()
+    cmd = f"SELECT name FROM sqlite_master WHERE type='table' AND name='{TABLE_NAME};'"
+    try:
+        res = cur.execute(cmd).fetchone()
+        if (not res):
+            return False
+    except sqlite3.DatabaseError as e:
+        log(Status.ERR, "from table_exists: operation failed.", e)
+        raise sqlite3.OperationalError("could not execute command to find whether table exists.", 
+                                       f"Command: {cmd}")
+    except Exception as e:
+        log(Status.ERR, "from table_exists: unexpected error occurred", e)
+        raise Exception("from table_exists: ", e)
+    finally:
+        ccfree(con,cur)
+    log(Status.OK, f"from table_exists: table found {TABLE_NAME}.")
+    return True
 
 def insert(entry: list | tuple):
     con, cur = establish()
@@ -104,3 +140,22 @@ def insert(entry: list | tuple):
         ccfree(con,cur)
     log(Status.OK, f"inserted values into {TABLE_NAME}.")
 
+def select_contest(cid: int) -> tuple:
+    con, cur = establish()
+    li = []
+    try:
+        cmd = f"SELECT {Attributes.INDEX.first} FROM {TABLE_NAME} WHERE cid == ?;"
+        res = cur.execute(cmd, (cid,)).fetchall()
+        if (not res):
+            return (False, li)
+        for line in res:
+            li.append(line[0])
+    except sqlite3.Error as e:
+        log(Status.ERR, f"could not get contest {cid} -", e)
+        raise sqlite3.OperationalError(f"Could not get contest {cid}.")
+    except Exception as e:
+        log(Status.ERR, f"an unexpected error occurred -", e)
+    finally:
+        ccfree(con,cur)
+    log(Status.OK, f"contest found.")
+    return (True, li)
