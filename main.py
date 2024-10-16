cce16fe9e44f9bb9a42e1cdaf0c6e75cc8275eee
XULIM <not3lbobillibo@gmail.com>
2024-10-16 19:29:50 -0400

    Code clean up for populate_table

--- main.py
+++ main.py
@@ -11,39 +11,26 @@ def exit_handler() -> None:
     print("Exiting Programme.")
 
 async def populate_table(forced: bool = False) -> void:
-    # ---
-    async def make():
-        db.create_table()
-        status, problems = await ps.get_problems()
-        if (status is Status.OK):
-            db.insert(problems)
-        else:
-            log(Status.ERR, "from populate_table: ", "could not populate table - problems.")
-            return
-        log(Status.OK, "from populate_table: ", "table population successful.")
-    # ---
-    if (not os.path.exists(db.DB_NAME)):
-        log(Status.WARN, "from populate_table:", f"file {db.DB_NAME} does not exist.")
-        try:
-            f = open(db.DB_NAME, "w")
-            f.write("")
-        except Exception as e:
-            log(Status.ERR, "from populate_table:", f"file {db.DB_NAME} cannot be created.", "aborting.")
-            return
-        finally:
-            f.close()
-        log(Status.OK, "from populate_table: ", f"file {db.DB_NAME} created.")
-        await make()
+    if (not forced and not file_refresh(db.DB_NAME, cd_days=3)):
+        log(Status.OK, "from populate_table:", f"file {db.DB_NAME} already exists.", "proceeding.")
         return
-    if (forced or file_refresh(db.DB_NAME, cd_days=3)):
-        db.drop_table()
-        await make()
+    log(Status.WARN, "from populate_table:",
+        f"file {db.DB_NAME} needs to be refreshed or the action is forced.",
+        f"proceeding with file refresh for {db.DB_NAME}."
+    )
+    # note that sqlite3 creates the file upon establishing a
+    # sqlite3.Connection to the file if it does not exist.
+    db.drop_table()
+    db.create_table()
+    status, problems = await ps.get_problems()
+    if (status is Status.OK):
+        db.insert(problems)
+    else:
+        log(Status.ERR, "from populate_table: ", "could not populate table - problems.")
         return
+    log(Status.OK, "from populate_table: ", "table population successful.")
 
 async def main() -> void:
-    os.remove(db.DB_NAME)
-    print(os.path.exists(db.DB_NAME))
-    con,cur = db.establish()
     try: 
         prob1 = {
             "contestId": 123,
@@ -54,17 +41,14 @@ async def main() -> void:
             "tags": ["implementation", "dp", "segment tree"]
         }
         entry = ps.dtot(prob1)
-        print(os.path.exists(db.DB_NAME))
-        # await populate_table()
-        # cid = 1420
-        # found, li = db.select_contest(cid)
-        # if (not found):
-        #     log(Status.WARN, f"could not find the contest {cid}.")
-        # else:
-        #     print(li)
+        await populate_table()
+        cid = 1420
+        found, li = db.select_contest(cid)
+        if (not found):
+            log(Status.WARN, f"could not find the contest {cid}.")
+        else:
+            print(li)
     except Exception as e:
         log(Status.ERR, str(e))
-    finally:
-        db.ccfree(con,cur)
 
 asyncio.run(main())
