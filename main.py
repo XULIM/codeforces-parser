6deebf709be3ee7088a77dd77e71dc89508f5819
XULIM <not3lbobillibo@gmail.com>
2024-10-24 16:05:22 -0400

    Update imports

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import atexit
 import asyncio
-from lib import db, ps
+from lib import db, ps, consts
 from lib.ps import Status, log, file_refresh
 
 type void = None
@@ -11,12 +11,12 @@ def exit_handler() -> None:
     print("Exiting Programme.")
 
 async def populate_table(forced: bool = False) -> void:
-    if (not forced and not file_refresh(db.DB_NAME, cd_days=3)):
-        log(Status.OK, "from populate_table:", f"file {db.DB_NAME} already exists.", "proceeding.")
+    if (not forced and not file_refresh(consts.DB_PATH, cd_days=3)):
+        log(Status.OK, "from populate_table:", f"file {consts.DB_PATH} already exists.", "proceeding.")
         return
     log(Status.WARN, "from populate_table:",
-        f"file {db.DB_NAME} needs to be refreshed or the action is forced.",
-        f"proceeding with file refresh for {db.DB_NAME}."
+        f"file {consts.DB_PATH} needs to be refreshed or the action is forced.",
+        f"proceeding with file refresh for {consts.DB_PATH}."
     )
     # note that sqlite3 creates the file upon establishing a
     # sqlite3.Connection to the file if it does not exist.
@@ -40,14 +40,8 @@ async def main() -> void:
             "rating": 1500,
             "tags": ["implementation", "dp", "segment tree"]
         }
-        entry = ps.dtot(prob1)
-        await populate_table()
-        cid = 1420
-        found, li = db.select_contest(cid)
-        if (not found):
-            log(Status.WARN, f"could not find the contest {cid}.")
-        else:
-            print(li)
+        curr_dir = "./"
+        print(os.listdir(curr_dir))
     except Exception as e:
         log(Status.ERR, str(e))
 
