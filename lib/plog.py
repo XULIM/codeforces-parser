3ca31bc8d6b75ed3a2fbafa0f66bfdff1b7055b6
XULIM <not3lbobillibo@gmail.com>
2024-10-25 14:28:12 -0400

    Add custom error object

--- lib/plog.py
+++ lib/plog.py
@@ -10,8 +10,18 @@ class Status(Enum):
     def __str__(self):
         return self.string
 
-def log(stat: Status, msg="", *args):
+class PError(Exception):
+    def __init__(self, msg="", *args):
+        errmsg: str = f"{str(Status.ERR)}::{msg}"
+        for arg in args:
+            errmsg += ("\n\t>> " + arg)
+        super().__init__(errmsg)
+
+def log(stat: Status | PError, msg="", *args):
     """
     Could throw an error if the passed in args does not have a valid __str__ method.
     """
+    if stat is PError:
+        print(stat)
+        return
     print(f"{str(stat)}::{msg}", *args, sep="\n\t>> ")
